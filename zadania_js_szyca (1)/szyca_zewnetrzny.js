d68be0d5ec2f7681d60b8aa2db2fcb2e3e0b2581
OS2123 <olimusztarda@gmail.com>
2025-09-21 15:59:03 +0200

    Delete zadania_js_szyca (1) directory

--- zadania_js_szyca (1)/szyca_zewnetrzny.js
+++
@@ -1,4 +0,0 @@
-// szyca_zewnetrzny.js - zewnętrzny skrypt dla Zadania 2
-// Wyświetla napis "ostatnia modyfikacja strony" oraz document.lastModified
-document.write("ostatnia modyfikacja strony".fontcolor("red").bold().fontsize(7) + "<br>");
-document.write(document.lastModified);
